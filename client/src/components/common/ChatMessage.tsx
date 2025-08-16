0c0d67d69441a3e6092d2c5dde0d765f9de14302
Caique Eduardo <caiqueworkti@gmail.com>
2025-08-16 00:42:26 -0300

    Update ChatMessage.tsx

--- client/src/components/common/ChatMessage.tsx
+++ client/src/components/common/ChatMessage.tsx
@@ -1,1 +1,15 @@
+import React from "react";
+
+interface ChatMessageProps {
+  user: string;
+  message: string;
+}
+
+export default function ChatMessage({ user, message }: ChatMessageProps) {
+  return (
+    <div className="p-2 border-b border-gray-200">
+      <strong>{user}:</strong> <span>{message}</span>
+    </div>
+  );
+}
 
